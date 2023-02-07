d1f5ba3c86abeec1fd02a048ec958e2d8d9bd562
RobWar-code <rowaniar@aol.com>
2023-02-07 20:20:56 +0000

    Add api status display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
-const API_STRING = "AzkVzENpL2XoEMGh8JIvLXoFiAE";
+const API_KEY = "AzkVzENpL2XoEMGh8JIvLXoFiAE";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultsModal = bootstrap.Modal(document.getElementById("resultsModal"));
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_URL}?key=${API_KEY}`;
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
     const response = await fetch(queryString);
     const data = await response.json();
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
     }
+    else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let apiStatus = data.status;
+    let heading = "API Key Status";
+    let result = `<div>Your key is valid until:</div>`;
+    result += `<div class="key-status">${data.expiry}</div>`
+    let modalTitle = document.getElementById("resultsModalTitle");
+    let modalBody = document.getElementById("results-content");
+    modalTitle.innerHTML = heading;
+    modalBody.innerHTML = result;
+
+    resultsModal.show();
 }

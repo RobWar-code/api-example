f8bcaa1d70187b6be1871824030c462a29d344b0
RobWar-code <rowaniar@aol.com>
2023-02-07 18:32:22 +0000

    Add initial javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-AzkVzENpL2XoEMGh8JIvLXoFiAE
+const API_STRING = "AzkVzENpL2XoEMGh8JIvLXoFiAE";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if (response.ok) {
+        console.log(data);
+    }
+}
